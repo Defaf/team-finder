bf2f34b954d0f3bd90370d885eb32849e601f653
Defaf <defaf_kamfar@hotmail.com>
2019-06-15 22:25:02 +0300

    install bootstrap library

--- finderApp/config/routes.rb
+++ finderApp/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   resources :events
   devise_for :users
+   # In `config/boot.rb`
+   ENV['EXECJS_RUNTIME'] = 'Node'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home", to: "home#index"
   root to: "home#index"
