67fba609f011111be6827bc36a4d20b099295d06
Defaf <defaf_kamfar@hotmail.com>
2019-06-16 16:26:05 +0300

    Create scaffold Team table

--- finderApp/config/routes.rb
+++ finderApp/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :teams
   resources :events
   devise_for :users
    # In `config/boot.rb`
--- finderApp/db/schema.rb
+++ finderApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_151758) do
+ActiveRecord::Schema.define(version: 2019_06_16_132217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2019_06_15_151758) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.integer "members"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_teams_on_event_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +51,5 @@ ActiveRecord::Schema.define(version: 2019_06_15_151758) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "teams", "events"
 end
