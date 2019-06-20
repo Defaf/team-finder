3287db11ee21e46bebdd1037ef2629c682937f65
Defaf <defaf_kamfar@hotmail.com>
2019-06-20 09:50:50 +0300

    Styling team pages

--- finderApp/app/models/team.rb
+++ finderApp/app/models/team.rb
@@ -3,5 +3,7 @@ class Team < ApplicationRecord
   belongs_to :user
   has_many :members
   has_many :users, through: :members
-  validates_numericality_of :num_members, less_than: 2
+  validates_length_of :name, minimum: 2 , maximum: 30
+  validates :name , uniqueness: true 
+  validates_numericality_of :num_members, greater_than: 1, less_than:7
 end
