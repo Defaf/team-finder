b19e2b7b8a8684a21922c153a6d51d02fd283fb9
Defaf <defaf_kamfar@hotmail.com>
2019-06-20 09:56:35 +0300

    Styling event pages

--- finderApp/app/models/event.rb
+++ finderApp/app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
     belongs_to :user 
     has_many :teams
+
+    validates :nameEvent, :date, :time , presence: true 
+
+    validates_length_of :nameEvent, minimum: 10 , maximum: 50
+    validates_length_of :description , maximum: 300
+    validates_length_of :duration , maximum: 30
+    validates_length_of :location,  minimum: 10 , maximum: 30
+
+
+
 end
