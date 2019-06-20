e8c7dcbad83ff620f1345a2b7c65c014485250c3
Defaf <defaf_kamfar@hotmail.com>
2019-06-20 09:58:03 +0300

    Styling user pages

--- finderApp/app/models/user.rb
+++ finderApp/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :teams
   has_many :members
   has_many :teams, through: :members
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
